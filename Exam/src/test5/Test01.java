b61096fab59746c35e652db10c73aad13e8503eb
sunnet123 <113005320+sunnet123@users.noreply.github.com>
2024-01-26 11:22:04 +0900

    자바 총정리 연습문제

---
+++ Exam/src/test5/Test01.java
@@ -0,0 +1,41 @@
+package test5;
+
+/*
+ * 날짜 : 2024/01/26
+ * 이름 : 최형욱
+ * 내용 : 자바 총정리 연습문제
+ */
+public class Test01 {
+	public static void main(String[] args) {
+		char var1 	= 'A';
+		int var2 	= var1;
+		char var3 	= 66;
+		int var4	= var1 + var3;
+		String var5 = """ 
+				Hello World!
+				Hello Korea!
+				Hello Busan!
+					""";
+
+		System.out.println("var1 : " + var1);
+		System.out.println("var2 : " + var2);
+		System.out.println("var3 : " + var3);
+		System.out.println("var4 : " + var4);
+		System.out.println(var5);
+		int a = 10;
+		int b = 5;
+		System.out.println(a > b || a == b);
+		System.out.println(a < b && a == b);
+		System.out.println(a > b ^ a == 10);
+		System.out.println(a > b ^ a == b);
+		System.out.println(a == b & test(1));
+		System.out.println(a == b && test(2));
+		System.out.println(b > 0 & (a / b > 0));
+		System.out.println(b > 0 && (a / b > 0));
+		}
+	
+		public static boolean test(int n) {
+		System.out.println("test(" + n + ") 실행...");
+		return true;
+	}
+}
